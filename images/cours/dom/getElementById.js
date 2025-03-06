474dea1e4340a29624da75877b8343502f1f4a6b
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2025-03-06 22:47:01 +0300

    Remove all files

--- images/cours/dom/getElementById.js
+++
@@ -1,6 +0,0 @@
-document.rootElement.addEventListener('click', myAlert, false);
-
-function myAlert(){
-	ellipse = document.getElementById('ellipse');
-	alert(ellipse);
-	}
--- images/cours/dom/parentNode.js
+++
@@ -1,9 +0,0 @@
-// constantes
-const svgNS = 'http://www.w3.org/2000/svg';
-
-document.rootElement.addEventListener('click', myAlert, false);
-
-function myAlert(){
-	var g2 = document.getElementById('g2');
-	alert('Père de g2 : ' + g2.parentNode);
-}
