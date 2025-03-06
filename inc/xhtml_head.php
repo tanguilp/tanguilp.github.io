c1651b64ee076aee446fe62559eaaf6f2f0a6045
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2025-03-06 22:15:21 +0300

    Delete PHP site

--- inc/xhtml_head.php
+++
@@ -1,3 +0,0 @@
-<link rel="icon" type="image/x-icon" href="images/svground.ico"/>
-<link rel="stylesheet" type="text/css" href="svground-style.css.php" title="SVGround : thème principal" media="screen"/>
-<link rel="stylesheet" type="text/css" href="svground-print.css.php" title="SVGround : thème principal" media="print"/>
